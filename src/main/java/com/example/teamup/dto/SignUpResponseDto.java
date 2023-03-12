1028604747f16ee9dd6ba2d37bfbcc2ae885a44f
cjoraeki <cjoraeki3@gmail.com>
2023-03-12 16:31:21 +0100

    boilerplate done

--- src/main/java/com/example/teamup/dto/SignUpResponseDto.java
+++ src/main/java/com/example/teamup/dto/SignUpResponseDto.java
@@ -5,8 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class SignUpResponseDto {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String city;
 }
